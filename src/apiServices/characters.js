b77c5ea4d4b866c19be52fbc82ff600b73fb7001
Fernando Barboza <falandocomofernando@gmail.com>
2021-03-12 13:06:49 -0300

    fix: project architecture

--- src/apiServices/characters.js
+++ src/apiServices/characters.js
@@ -5,40 +5,7 @@ const api = require('./axios');
 *
 * @param name string
 */
-const getByName = async (name) => {
-  const character = {};
-
-  try {
-    const response = await api.get(
-      `characters/${name}.json`
-    );
-  
-    const { characters } = response.data;
-
-    if (characters.error) return false;
-  
-    character.name = characters.data.name;
-    character.title = characters.data.title;
-    character.sex = characters.data.sex;
-    character.vocation = characters.data.vocation;
-    character.level = characters.data.level;
-    character.achievementPoints = characters.data.achievement_points;
-    character.world = characters.data.world;
-    character.residence = characters.data.residence;
-    character.lastLogin = characters.data.last_login;
-    character.accountStatus = characters.data.account_status;
-    character.status = characters.data.status;
-    character.achievements = characters.achievements;
-    character.deaths = characters.deaths;
-    character.accountInformation = characters.account_information;
-  
-    return character;
-
-  } catch (error) {
-    console.log("Error: " + error);
-    return false;
-  }
-}
+const getByName = async (name) => await api.get(`characters/${name}.json`);
 
 module.exports = {
   getByName
--- src/applicationServices/character.js
+++ src/applicationServices/character.js
@@ -15,7 +15,34 @@ const handleCharacterName = async (name) => {
     characterName = characterNameSplited.join("");
   }
 
-  return characterApiService.getByName(characterName);
+  return characterApiService.getByName(characterName)
+    .then(({ data }) => {
+      const character = {};
+
+      const { characters } = data;
+
+      if (characters.error) return false;
+    
+      character.name = characters.data.name;
+      character.title = characters.data.title;
+      character.sex = characters.data.sex;
+      character.vocation = characters.data.vocation;
+      character.level = characters.data.level;
+      character.achievementPoints = characters.data.achievement_points;
+      character.world = characters.data.world;
+      character.residence = characters.data.residence;
+      character.lastLogin = characters.data.last_login;
+      character.accountStatus = characters.data.account_status;
+      character.status = characters.data.status;
+      character.achievements = characters.achievements;
+      character.deaths = characters.deaths;
+      character.accountInformation = characters.account_information;
+    
+      return character;
+    })
+    .catch(({ response }) => {
+      console.log("ERROR: " + response)
+    });
 }
 
 module.exports = {
